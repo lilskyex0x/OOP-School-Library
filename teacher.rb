c1d440bd03ef0e344dc403de824f9ae0f3680a69
unknown <lilskyex0x.apk@gmail.com>
2023-11-07 19:31:09 +0630

    Create teacher class

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require_relative 'person'
+
+class Teacher < Person
+  attr_reader :specialization
+
+  def initialize(id, specialization, age = 0, name: "Unknown", parent_permission: true)
+    super(id, age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
