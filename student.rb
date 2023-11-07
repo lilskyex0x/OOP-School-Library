023b42bb1b7419c641e69df551bc12109f7eda96
unknown <lilskyex0x.apk@gmail.com>
2023-11-07 19:32:11 +0630

    Fix linter errors

--- student.rb
+++ student.rb
@@ -3,12 +3,12 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(id, classroom, age = 0, name: "Unknown", parent_permission: true)
+  def initialize(id, classroom, age = 0, name: 'Unknown', parent_permission: true)
     super(id, age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\\_(ツ)_/¯"
+    '¯\\_(ツ)_/¯'
   end
 end
--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(id, specialization, age = 0, name: "Unknown", parent_permission: true)
+  def initialize(id, specialization, age = 0, name: 'Unknown', parent_permission: true)
     super(id, age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
