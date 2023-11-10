3c9069bd56d6ccd3ecaafd60d6cf677c27b8d658
unknown <lilskyex0x.apk@gmail.com>
2023-11-10 13:41:34 +0630

    Modify person class

--- person.rb
+++ person.rb
@@ -1,7 +1,8 @@
 require_relative 'nameable'
+
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(id, age = 0, name: 'Unknown', parent_permission: true)
     super(id)
@@ -9,10 +10,18 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def rent_book(book_title, book_author, date)
+    book = Book.new(book_title, book_author)
+    rental = Rental.new(date, self, book)
+    @rentals << rental
+    rental
   end
 
   def of_age?
@@ -21,5 +30,29 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  class Book
+    attr_accessor :title, :author
+    attr_reader :rentals
+
+    def initialize(title, author)
+      @title = title
+      @author = author
+      @rentals = []
+    end
+  end
+
+  class Rental
+    attr_accessor :date
+    attr_reader :person, :book
+
+    def initialize(date, person, book)
+      @date = date
+      @person = person
+      @book = book
+      person.rentals << self
+      book.rentals << self
+    end
   end
 end
