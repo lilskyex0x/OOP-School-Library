4515c2fe47aeee39fd1269ff6cfe9a9f0bbf06ae
unknown <lilskyex0x.apk@gmail.com>
2023-11-10 13:42:15 +0630

    Add id in the constructor of nameable class

--- nameable.rb
+++ nameable.rb
@@ -1,4 +1,10 @@
 class Nameable
+  attr_reader :id
+
+  def initialize(id)
+    @id = id
+  end
+
   def correct_name
     raise NotImplementedError, "The method 'correct_name' must be implemented in a subclass."
   end
